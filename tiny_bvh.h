ef12f2d162ba0f28e72a48319f845bbcd84ef18a
jbikker <bikker.j@gmail.com>
2025-06-07 13:53:49 +0200

    Dev is now 1.5.7.

--- tiny_bvh.h
+++ tiny_bvh.h
@@ -91,7 +91,7 @@ THE SOFTWARE.
 // Library version:
 #define TINY_BVH_VERSION_MAJOR	1
 #define TINY_BVH_VERSION_MINOR	5
-#define TINY_BVH_VERSION_SUB	6
+#define TINY_BVH_VERSION_SUB	7
 
 // Run-time checks; disabled by default.
 // #define PARANOID // checks out-of-bound access of slices
