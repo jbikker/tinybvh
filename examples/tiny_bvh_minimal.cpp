1e39a12196e70f4a480189b02d9d848564a17450
Jacco Bikker <bikker.j@protonmail.com>
2026-08-27 14:59:02 +0200

    Minor improvements to example code.

--- examples/tiny_bvh_minimal.cpp
+++ examples/tiny_bvh_minimal.cpp
@@ -1,8 +1,8 @@
 // Minimal example for TinyBVH.
 // The code in main generates a collection of random triangles.
-// All triangles are specified as 3 vertices; each vertex is a 'vec4'. 
-// A Ray is constructed using an origin and direction.
-// A BVH is instantiated and constructed using BVH::Build( .. ). 
+// All triangles are specified as 3 vertices; each vertex is a 'bvhvec4'. 
+// A tinybvh::Ray is constructed using an origin and direction.
+// A tinybvh::BVH is instantiated and constructed using BVH::Build( .. ). 
 // A ray is traced using BVH::Intersect( .. ).
 // The intersection result is obtained from the ray.hit record.
 
@@ -11,9 +11,8 @@
 #include <cstdlib>
 #include <cstdio>
 
-#define TRIANGLE_COUNT	8192
-
-tinybvh::bvhvec4 triangles[TRIANGLE_COUNT * 3]; // must be 16 byte!
+static constexpr uint32_t TRIANGLE_COUNT = 8192;
+static tinybvh::bvhvec4 vertices[TRIANGLE_COUNT * 3]; // must be 16 byte!
 
 float uniform_rand() { return (float)rand() / (float)RAND_MAX; }
 
@@ -23,9 +22,9 @@ int main()
 	for (int i = 0; i < TRIANGLE_COUNT; i++)
 	{
 		// create a random triangle
-		tinybvh::bvhvec4& v0 = triangles[i * 3 + 0];
-		tinybvh::bvhvec4& v1 = triangles[i * 3 + 1];
-		tinybvh::bvhvec4& v2 = triangles[i * 3 + 2];
+		tinybvh::bvhvec4& v0 = vertices[i * 3 + 0];
+		tinybvh::bvhvec4& v1 = vertices[i * 3 + 1];
+		tinybvh::bvhvec4& v2 = vertices[i * 3 + 2];
 		// triangle position, x/y/z = 0..1
 		float x = uniform_rand();
 		float y = uniform_rand();
@@ -44,17 +43,21 @@ int main()
 		v2.z = z + 0.1f * uniform_rand();
 	}
 
+	// construct a ray
 	tinybvh::bvhvec3 O( 0.5f, 0.5f, -1 );
 	tinybvh::bvhvec3 D( 0.1f, 0, 2 );
 	tinybvh::Ray ray( O, D );
 
 	// build a BVH over the scene
 	tinybvh::BVH bvh;
-	bvh.Build( triangles, TRIANGLE_COUNT );
+	bvh.Build( vertices, TRIANGLE_COUNT );
 
 	// from here: play with the BVH!
 	int steps = bvh.Intersect( ray );
-	printf( "std: nearest intersection: %f (found in %i traversal steps).\n", ray.hit.t, steps );
+	if (ray.hit.t < BVH_FAR)
+		printf( "hit prim %u at t=%f (%i steps)\n", ray.hit.prim, ray.hit.t, steps );
+	else
+		printf( "no hit (%i steps)\n", steps );
 
 	// all done.
 	return 0;
--- examples/tiny_bvh_viewer.cpp
+++ examples/tiny_bvh_viewer.cpp
@@ -1,10 +1,11 @@
-// Minimalistic object viewer application.
-// Useful for inspecting BVHs and for finding camera views for the benchmark app.
+// Minimalistic object viewer application:
+// Renders using CPU ray tracing.
+// Uses 'Fenster' for cross-platform windowed graphics.
+// Any of the BVH layouts with an Intersect method can be used.
 
 #define FENSTER_APP_IMPLEMENTATION
 #define SCRWIDTH 800
 #define SCRHEIGHT 800
-#define TILESIZE 20
 #include "external/fenster.h" // https://github.com/zserge/fenster
 
 #define TINYBVH_IMPLEMENTATION
@@ -18,60 +19,59 @@ using namespace tinybvh;
 
 // Application variables
 
-static BVH_GPU bvh;
-static bvhvec4* tris = 0;
-static int triCount = 0;
-static std::atomic<int> tileIdx( 0 );
-static unsigned threadCount = std::thread::hardware_concurrency();
+BVH_GPU bvh;							// BVH layout to use. Can be: BVH, BVH4_CPU, BVH8_CPU, BVH8_CWBVH.
+bvhvec4* verts = 0;						// Triangle data. Three bvhvec4 vertices per triangle.
+int triCount = 0;						// Triangle count.
+std::atomic<int> tileIdx( 0 );			// Atomic counter for tile-based threaded rendering.
 
 // View pyramid for a pinhole camera
-static bvhvec3 eye( 0, 30, 0 ), p1, p2, p3;
-static bvhvec3 view = tinybvh_normalize( bvhvec3( -1, 0, 0 ) );
+bvhvec3 eye( 0, 30, 0 ), p1, p2, p3;	// View pyramid; p1 is top-left, p2 is top-right, p3 is bottom-left.
+bvhvec3 view( -1, 0, 0 );				// Initial view direction.
 
-// Geometry access
-bvhvec3 rgb32_to_vec3( const unsigned c ) { return bvhvec3( (float)(c >> 16), (float)((c >> 8) & 255), (float)(c & 255) ) * (1 / 255.f); }
-bvhvec3 TriangleColor( const unsigned idx ) { return rgb32_to_vec3( *(unsigned*)&tris[idx * 3].w ); }
-bvhvec3 TriangleNormal( const unsigned idx )
-{
-	bvhvec3 a = tris[idx * 3], b = tris[idx * 3 + 1], c = tris[idx * 3 + 2];
-	return tinybvh_normalize( tinybvh_cross( b - a, a - c ) );
-}
-
-// Scene management - Append a file, with optional position, scale and color override, tinyfied
+// Scene management - Append a file, with optional position, scale and color override, 'tinyfied'
 void AddMesh( const char* file, float scale = 1, bvhvec3 pos = {}, int c = 0, int N = 0 )
 {
 	std::fstream s{ file, s.binary | s.in };
 	s.read( (char*)&N, 4 );
-	bvhvec4* data = (bvhvec4*)malloc64( (N + triCount) * 48 );
-	if (tris) memcpy( data, tris, triCount * 48 ), free64( tris );
-	tris = data, s.read( (char*)tris + triCount * 48, N * 48 ), triCount += N;
-	for (int* b = (int*)tris + (triCount - N) * 12, i = 0; i < N * 3; i++)
+	bvhvec4* data = (bvhvec4*)malloc64( (size_t)(N + triCount) * 48 );
+	if (verts) memcpy( data, verts, triCount * 48 ), free64( verts );
+	verts = data, s.read( (char*)verts + triCount * 48, N * 48 ), triCount += N;
+	for (int* b = (int*)verts + (triCount - N) * 12, i = 0; i < N * 3; i++)
 		*(bvhvec3*)b = *(bvhvec3*)b * scale + pos, b[3] = c ? c : b[3], b += 4;
 }
 
-// Application init
+// Geometry access
+bvhvec3 rgb32_to_vec3( const unsigned c ) { return bvhvec3( (float)(c >> 16), (float)((c >> 8) & 255), (float)(c & 255) ) * (1 / 255.f); }
+bvhvec3 TriangleColor( const unsigned idx ) { return rgb32_to_vec3( *(unsigned*)&verts[idx * 3].w ); }
+bvhvec3 TriangleNormal( const unsigned idx )
+{
+	bvhvec3 a = verts[idx * 3], b = verts[idx * 3 + 1], c = verts[idx * 3 + 2];
+	return tinybvh_normalize( tinybvh_cross( b - a, a - c ) );
+}
+
+// Application initialization
 void Init()
 {
 	// prepare scene
-#if 0
-	AddMesh( "./testdata/bistro_ext_part1.bin" );
-	AddMesh( "./testdata/bistro_ext_part2.bin" );
+#if 1
+	AddMesh( "./testdata/cryteksponza.bin" );			// Load one .bin file with triangle data,
 #else
-	AddMesh( "./testdata/cryteksponza.bin" );
+	AddMesh( "./testdata/bistro_ext_part1.bin" );		// ..or several.
+	AddMesh( "./testdata/bistro_ext_part2.bin" );		// See ./testdata for a collection of meshes.
 #endif
-	bvh.BuildHQ( tris, triCount );
-	// load camera position / direction from file
-	std::fstream t = std::fstream{ "camera.bin", t.binary | t.in };
-	if (!t.is_open()) return;
-	t.read( (char*)&eye, sizeof( eye ) );
-	t.read( (char*)&view, sizeof( view ) );
+	bvh.BuildHQ( verts, triCount );						// Construct a high-quality BVH.
+	// load serialized camera position / direction from file
+	std::fstream s = std::fstream{ "camera.bin", s.binary | s.in };
+	if (!s.is_open()) return;
+	s.read( (char*)&eye, sizeof( eye ) );
+	s.read( (char*)&view, sizeof( view ) );
 }
 
-// Keyboard handling
+// Keyboard handling: WASD for movement, R,F: up/down, cursors: rotate.
 void UpdateCamera( float dt, fenster& f )
 {
-	bvhvec3 right = tinybvh_normalize( tinybvh_cross( bvhvec3( 0, 1, 0 ), view ) ), up = 0.8f * tinybvh_cross( view, right );
 	// get camera controls.
+	static bvhvec3 right, up;
 	if (f.keys['A']) eye += right * -1.0f * dt * 10; else if (f.keys['D']) eye += right * dt * 10;
 	if (f.keys['W']) eye += view * dt * 10; else if (f.keys['S']) eye += view * -1.0f * dt * 10;
 	if (f.keys['R']) eye += up * dt * 20; else if (f.keys['F']) eye += up * -1.0f * dt * 20;
@@ -79,28 +79,18 @@ void UpdateCamera( float dt, fenster& f )
 	if (f.keys[19]) view = tinybvh_normalize( view + right * dt );
 	if (f.keys[17]) view = tinybvh_normalize( view + up * -1.0f * dt );
 	if (f.keys[18]) view = tinybvh_normalize( view + up * dt );
-	// save camera position
-	static bool sdown = true;
-	if (!f.keys['P']) sdown = false; else if (!sdown)
-	{
-		sdown = true;
-		FILE* f = fopen( "cam.txt", "a" );
-		fprintf( f, "camPos[0] = bvhvec3( %.2f, %.2f, %.2f ), camDir[0] = tinybvh_normalize( bvhvec3( %.2f, %.2f, %.2f ) );\n",
-			eye.x, eye.y, eye.z, view.x, view.y, view.z ); 
-		fclose( f );
-	}
 	// recalculate right, up
 	right = tinybvh_normalize( tinybvh_cross( bvhvec3( 0, 1, 0 ), view ) ), up = 0.8f * tinybvh_cross( view, right );
-	bvhvec3 C = eye + 1.2f * view;
-	p1 = C - right + up, p2 = C + right + up, p3 = C - right - up;
+	bvhvec3 center = eye + 1.2f * view;
+	p1 = center - right + up, p2 = center + right + up, p3 = center - right - up;
 }
 
+// Worker thread renders tiles until no unclaimed tiles are left.
 void TraceWorkerThread( uint32_t* buf, int threadIdx )
 {
+	static constexpr uint32_t TILESIZE = 20;
 	const int xtiles = SCRWIDTH / TILESIZE, ytiles = SCRHEIGHT / TILESIZE;
-	const int tiles = xtiles * ytiles;
-	int tile = threadIdx;
-	while (tile < tiles)
+	for (int tiles = xtiles * ytiles, tile = threadIdx; tile < tiles; tile = tileIdx++)
 	{
 		const int tx = tile % xtiles, ty = tile / xtiles;
 		for (int y = 0; y < TILESIZE; y++) for (int x = 0; x < TILESIZE; x++)
@@ -113,19 +103,17 @@ void TraceWorkerThread( uint32_t* buf, int threadIdx )
 			// trace
 			Ray ray( eye, D );
 			bvh.Intersect( ray );
-			bvhvec3 E( 0.6f, 0.7f, 1 );
-			if (ray.hit.t < 1e30f)
-			{	
-				bvhvec3 N = TriangleNormal( ray.hit.prim );
+			bvhvec3 E( 0.6f, 0.7f, 1 ); // sky color
+			if (ray.hit.t < BVH_FAR)
+			{
+				bvhvec3 N = TriangleNormal( ray.hit.prim & PRIM_IDX_MASK );
 				if (tinybvh_dot( N, ray.D ) > 0) N *= -1.0f;
-				E = (N + 1) * 0.5f;
+				E = (N + 1) * 0.5f; // encode normal as rgb color
 			}
-			const int r = (int)tinybvh_min( 255.0f, sqrtf( E.x ) * 255.0f );
-			const int g = (int)tinybvh_min( 255.0f, sqrtf( E.y ) * 255.0f );
-			const int b = (int)tinybvh_min( 255.0f, sqrtf( E.z ) * 255.0f );
-			buf[pixelIdx] = b + (g << 8) + (r << 16);
+			float r = 255.0f * E.x, g = 255.0f * E.y, b = 255.0f * E.z;
+			// store color in Fenster pixel buffer
+			buf[pixelIdx] = (int)b + ((int)g << 8) + ((int)r << 16);
 		}
-		tile = tileIdx++;
 	}
 }
 
@@ -133,12 +121,14 @@ void TraceWorkerThread( uint32_t* buf, int threadIdx )
 void Tick( float delta_time_s, fenster& f, uint32_t* buf )
 {
 	UpdateCamera( delta_time_s, f );
-	tileIdx = threadCount;
+	const uint32_t cores = tinybvh_max( 1u, std::thread::hardware_concurrency() );
+	tileIdx = cores;
 	std::vector<std::thread> threads;
-	for (uint32_t i = 0; i < threadCount; i++) threads.emplace_back( &TraceWorkerThread, buf, i );
+	for (uint32_t i = 0; i < cores; i++) threads.emplace_back( &TraceWorkerThread, buf, i );
 	for (auto& thread : threads) thread.join();
+	// use Fenster window title to display camera parameters and fps
 	char title[256];
-	snprintf( title, 256, "cam (%.2f,%.2f,%.2f) => (%.2f,%.2f,%.2f) - press P to save to cam.txt",
+	snprintf( title, sizeof( title ), "cam (%.2f,%.2f,%.2f) => (%.2f,%.2f,%.2f)",
 		eye.x, eye.y, eye.z, view.x, view.y, view.z );
 	fenster_update_title( &f, title );
 }
